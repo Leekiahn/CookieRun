5c0ef15604b68b39c07ec977c9b8da1298778069
Leekiahn <kiahn2000@gmail.com>
2025-05-08 15:25:42 +0900

    update : Unit.cs, PlayerController.cs 작성

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : Unit
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        Hp = 100f;
+        Speed = 3f;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 dir = new Vector3(horizontal, vertical).normalized;
         
+        Move(dir);
+    }
+
+    protected override void Move(Vector3 dir)
+    {
+        base.Move(dir);
     }
 }
--- Assets/Scripts/Player/Unit.cs
+++ Assets/Scripts/Player/Unit.cs
@@ -1,8 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    //최대 체력
+    private const float fullHP = 100f;
     
+    //HP
+    [SerializeField] private float hp;
+    protected float Hp
+    {
+        get { return hp; }
+        set
+        {
+            hp = Mathf.Clamp(value, 0, fullHP);
+        }
+    }
+    
+    //스피드
+    [SerializeField] private float speed;
+    protected float Speed
+    {
+        get { return speed; }
+        set
+        {
+            speed = Mathf.Max(0, value);
+        }
+    }
+
+    protected virtual void Move(Vector3 dir)
+    {
+        transform.position += dir * speed * Time.deltaTime;
+    }
 }
